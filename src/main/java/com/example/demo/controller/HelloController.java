d691340a3d2e3f91638d4ef363c3c98a12efd910
Damian Juzyk <damian.juzyk@gmail.com>
2018-11-09 17:59:39 +0100

    add autowired do HelloController

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.RandomService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/aplikacja")
 public class HelloController {
 
+    private RandomService randomService;
+
+    public HelloController(RandomService randomService) {
+        this.randomService = randomService;
+    }
+
+
     //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
 //wskazanie strony oraz danej metody(get, post, delete..);
 
@@ -20,7 +28,7 @@ public class HelloController {
     //nie trzeba okreslac metody tylko sam adres
     public String hello() {
         System.out.println("Wykonanie metody hello");
-        return "Wykonanie metody hello";
+        return "Wykonanie metody hello" + " " + randomService.getSmallInt();
     }
 
     @GetMapping("/example")
